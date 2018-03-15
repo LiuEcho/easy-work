c1b303861b164b0bcb6ba3d6621013b21635ca59
liu chang <lc7906@qq.com>
2018-03-15 09:50:09 +0800

    first commit
    方便工作

---
+++ createMyVueFileQuick.js
@@ -0,0 +1,51 @@
+const fs = require('fs')
+const path = require('path').resolve('./')
+const defaults = {
+  flags: 'w',
+  encoding: 'utf8',
+  fd: null,
+  mode: 0o666,
+  autoClose: true
+}
+
+let filePath = process.argv[2]
+let className = process.argv[3]
+let fileName = ''
+className.split('-').forEach(word => {
+  fileName += word.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase())
+})
+
+
+const writeFile = (className, fileName, filePath) => {
+  let content = 
+  `<template lang="html">
+    ${fileName}
+  </template>
+  <script>
+  import { showMessage } from 'utils/message'
+  import { TablePagination, ContentBox } from 'views/console'
+  export default {
+    name: '${fileName}',
+    components: {
+      TablePagination, ContentBox
+    },
+    data () {
+      return {
+      }
+    }
+  }
+  </script>
+  <style lang="css">
+  .${className} {
+
+  }
+  </style>
+  `
+  
+  let writeStream = fs.createWriteStream(`${path}/${filePath}/${fileName}.vue`, defaults)
+  writeStream.write(content, 'utf-8', () => {
+    console.log(`finish, created ${path}/${filePath}/${fileName}.vue`)
+  })
+}
+
+writeFile(className, fileName, filePath)
