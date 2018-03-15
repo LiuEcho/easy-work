550686383971baf61c917d6d315da11155e03272
liu chang <lc7906@qq.com>
2018-03-15 10:41:17 +0800

    add method mounted

--- createMyVueFileQuick.js
+++ createMyVueFileQuick.js
@@ -15,17 +15,24 @@ className.split('-').forEach(word => {
   fileName += word.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase())
 })
 
-
 const writeFile = (className, fileName, filePath) => {
-  let content = 
-  `<template lang="html">
+let content = 
+`<template lang="html">
   <div class="${className}">
-    ${fileName}
+    <content-box :titles="titles">
+      <div slot="monitor-contact">
+        ${fileName}
+        <el-table :data="${fileName}.tableData" v-loading="${fileName}.tableDataLoading" border>
+        </el-table>
+        <table-pagination :handleCurrentChange="get${fileName}" :pageConfig="${fileName}.pageConfig"></table-pagination>
+      </div>
+    </content-box>
   </div>
 </template>
 <script>
 import { showMessage } from 'utils/message'
 import { TablePagination, ContentBox } from 'views/console'
+import { get${fileName}, channelTest, channelMod } from ''
 export default {
   name: '${fileName}',
   components: {
@@ -33,7 +40,41 @@ export default {
   },
   data () {
     return {
+      titles: [
+        {
+          title: '${fileName}',
+          slotName: '${className}'
+        }
+      ],
+      ${fileName}: {
+        tableData: [],
+        tableDataLoading: true,
+        pageConfig: {
+          curPage: 1,
+          pageSize: 10,
+          total: 0
+        }
+      }
     }
+  },
+  methods: {
+    get${fileName} (curPage) {
+      this.${fileName}.tableDataLoading = true
+      this.${fileName}.searchData.curPage = curPage || this.${fileName}.pageConfig.curPage
+      get${fileName}(this.${fileName}.searchData).then(data => {
+        if (data.success) {
+          this.${fileName}.tableData = data.results
+          this.${fileName}.pageConfig.curPage = data.curPage
+          this.${fileName}.pageConfig.total = data.count
+          this.${fileName}.tableDataLoading = false
+        } else {
+          showMessage('error', data.message)
+        }
+      })
+    }
+  },
+  mounted () {
+    this.get${fileName}()
   }
 }
 </script>
